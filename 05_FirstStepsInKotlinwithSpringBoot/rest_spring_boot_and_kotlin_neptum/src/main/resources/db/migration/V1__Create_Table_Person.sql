88708bd62290e0ad9c75742df7765d796bcc1f7e
016765631 <matheus_adps@hotmail.com>
2023-04-20 10:26:15 -0300

    Migration com FlyWay; DozerMapper; Testes Unitário

---
+++ 05_FirstStepsInKotlinwithSpringBoot/rest_spring_boot_and_kotlin_neptum/src/main/resources/db/migration/V1__Create_Table_Person.sql
@@ -0,0 +1,13 @@
+--
+-- Table structure for table `person`
+--
+
+DROP TABLE IF EXISTS `person`;
+CREATE TABLE `person` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `address` varchar(100) NOT NULL,
+  `first_name` varchar(80) NOT NULL,
+  `gender` varchar(6) NOT NULL,
+  `last_name` varchar(80) NOT NULL,
+  PRIMARY KEY (`id`)
+)
---
+++ 05_FirstStepsInKotlinwithSpringBoot/rest_spring_boot_and_kotlin_neptum/src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,9 @@
+--
+-- Dumping data for table `person`
+--
+
+INSERT INTO `person` VALUES
+    (1,'Anchiano - Italy','Leonardo','Male','Da Vinci'),
+    (3,'Anchiano - Italy','Leonardo','Male','Da Vinci'),
+    (5,'Porbandar - India','Mahatma','Male','Gandhi'),
+    (7,'Porbandar - India','Indira','Female','Gandhi');
